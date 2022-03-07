e595d5998526bf0caa18c7a370c1c247be64c244
Heborine <seansilver562@gmail.com>
2022-03-07 14:47:35 -0800

    PID resources

--- Robotics_Team6000_2022_Programming/src/main/java/frc/robot/subsystems/PID.java
+++ Robotics_Team6000_2022_Programming/src/main/java/frc/robot/subsystems/PID.java
@@ -4,3 +4,5 @@ public class PID {
     
 }
 //http://wiki.carlmontrobotics.org/Java:_PID_Control
+//https://docs.wpilib.org/en/stable/docs/software/commandbased/pid-subsystems-commands.html#:~:text=Using%20a%20PIDSubsystem%20Once%20an%20instance%20of%20a,track%20to%20the%20setpoint%20using%20the%20defined%20output%3A
+//https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj/command/PIDSubsystem.html
