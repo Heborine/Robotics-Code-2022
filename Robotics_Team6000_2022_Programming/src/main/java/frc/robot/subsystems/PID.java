08d026c1cff0e1e4b736222d288b3a37ee9eb0e1
FIREBOTICS <shalhevetroboticsprogramming@gmail.com>
2022-03-22 16:18:07 -0700

    Update PID.java

--- Robotics_Team6000_2022_Programming/src/main/java/frc/robot/subsystems/PID.java
+++ Robotics_Team6000_2022_Programming/src/main/java/frc/robot/subsystems/PID.java
@@ -3,6 +3,15 @@ package frc.robot.subsystems;
 public class PID {
     
 }
+
+
+// If no Gyro, simple way is to make output = 1/distance
+
+//https://frc-pdr.readthedocs.io/en/latest/control/pid_control.html#proportional
+
+
+
+
 //http://wiki.carlmontrobotics.org/Java:_PID_Control
 //https://docs.wpilib.org/en/stable/docs/software/commandbased/pid-subsystems-commands.html#:~:text=Using%20a%20PIDSubsystem%20Once%20an%20instance%20of%20a,track%20to%20the%20setpoint%20using%20the%20defined%20output%3A
 //https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj/command/PIDSubsystem.html
